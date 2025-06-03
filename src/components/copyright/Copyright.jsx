fd11ec528676cf9508c027dfbb3069eff0dfb9c1
Felipe França <dev.felipefrancca@gmail.com>
2025-06-03 11:29:17 -0300

    Arquivos que faltaram ser enviados.

--- src/components/copyright/Copyright.jsx
+++ src/components/copyright/Copyright.jsx
@@ -2,9 +2,16 @@ import { Box, Link, Typography } from "@mui/material";
 
 export default function Copyright(props) {
   return (
-    <Box className="Copyright" sx={{display: "flex", justifyContent: "center", mt: 2}}>
+    <Box
+      className="Copyright"
+      sx={{ display: "flex", justifyContent: "center", mt: 2 }}
+    >
       <Typography>
-        <Link sx={{textDecoration: "none", color: "000"}} href="https://felipefrancca-portfolio.netlify.app" target="blank">
+        <Link
+          sx={{ textDecoration: "none", color: "000" }}
+          href="https://felipefrancca-portfolio.netlify.app"
+          target="blank"
+        >
           Felipe França
         </Link>{" "}
         {"Copyright "}
